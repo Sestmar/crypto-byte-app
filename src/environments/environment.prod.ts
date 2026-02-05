b9995134f2a0d0f0a6ec69537d4fd9e9db36f11d
Estu JR <sergio.estudilloo7@gmail.com>
2026-02-05 22:01:47 +0100

    Actualización en el idioma de la app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    projectId: "cryptobyte-1aad7",
+    appId: "1:953486025598:web:cf0ec693ed1c64b85c492f",
+    storageBucket: "cryptobyte-1aad7.firebasestorage.app",
+    apiKey: "AIzaSyCaW-nf1WCv6MMYYs6PjZuS23U8JSkHqRo",
+    authDomain: "cryptobyte-1aad7.firebaseapp.com",
+    messagingSenderId: "953486025598"
+  }
 };
